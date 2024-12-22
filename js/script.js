02027b50fe224a9ae7a617c11a76fc5519ec12d7
Hazem Hassan Mahmoud <hazemhassan0321@gmail.com>
2024-12-22 22:53:54 +0200

    Upload The Project

---
+++ js/script.js
@@ -0,0 +1,18 @@
+let toTop = document.getElementById("top");
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= 300) {
+    toTop.style.transform = "scale(1)";
+  } else {
+    toTop.style.transform = "scale(0)";
+  }
+});
+toTop.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
+
+let date = new Date();
+let copyright = document.getElementById("date");
+copyright.textContent = date.getFullYear();
